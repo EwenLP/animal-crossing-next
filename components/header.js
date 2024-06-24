88fa907d31941a1ce459e5e8ca4fa16c4240dc72
Ewen_lp <stagiaire-1@3codes.io>
2024-06-24 08:48:53 +0200

    Update header image

--- components/header.js
+++ components/header.js
@@ -2,8 +2,8 @@
 export default function Header() {
   return (
     <header className="container">
+      <img src="/Logo-ac.jpeg" alt="Animal Crossing Logo 2"/>
       <img src="/ac.png" alt="Animal Crossing logo"/>
-      <img src="/Logo-ac" alt="Animal Crossing Logo 2"/>
     </header>
   );
 }
